1097cacd3eb9b6cb92fe85086857c260f1c989dd
dbg <no>
2018-11-19 14:05:29 -0800

    survive various exceptions

--- public-charge/scraper/get_comments.py
+++ public-charge/scraper/get_comments.py
@@ -6,9 +6,12 @@ from sqlalchemy.exc import IntegrityError
 from math import ceil
 from xvfbwrapper import Xvfb
 from scraper import Scraper
+import sys
 
 Base = declarative_base()
 
+results_per_page = 50
+comment_delay = 1
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -20,9 +23,15 @@ class Comment(Base):
     date_received = Column(String, nullable=False)
     traking_number = Column(String, nullable=False)
     rin = Column(String, nullable=False)
-
+    phase = Column(String, nullable=False)
 
 if __name__ == "__main__":
+
+    page_start = 1
+    if len(sys.argv) > 1:
+        page_start = int(sys.argv[1])
+
+    print("Initializing ...")
 
     engine = create_engine('sqlite:///comments.sqlite')
     # Bind the engine to the metadata of the Base class so that the
@@ -38,43 +47,56 @@ if __name__ == "__main__":
     vdisplay.start()
 
     # Start scraper.
-    scraper = Scraper(delay=4)
+    scraper = Scraper(delay=comment_delay, results_per_page=results_per_page)
 
-    # Get the total number of comments.
-    total_comments = scraper.get_comments_total()
-    total_pages = ceil(total_comments / 50)  # 50 comments per page.
+    print("Scraping ...")
 
-    # Go through all pages.
-    for page in range(1, total_pages + 1):
-        # Get comment urls on given page.
-        urls = scraper.get_comments_urls_on_page(page)
+    try:
+        # Get the total number of comments.
+        total_comments = scraper.get_comments_total()
+        print("Total comments: {}".format(total_comments))
+        total_pages = ceil(total_comments / results_per_page)
+        print("Total pages: {}".format(total_pages))
 
-        for i, url in enumerate(urls):
-            # USCIS ID.
-            uscisid = url.split("=")[-1]
+        # Go through all pages.
+        for page in range(page_start, total_pages + 1):
+            # Get comment urls on given page.
+            urls = scraper.get_comments_urls_on_page(page)
+            print("Page {}: Urls: {}".format(page, len(urls)))
 
-            # Check if entry exists.
-            exists = session.query(Comment).filter(
-                Comment.uscisid == uscisid).first()
-            if exists is None:
-                c = scraper.scrape_comment(comment_url=url)
+            for i, url in enumerate(urls):
+                # USCIS ID.
+                uscisid = url.split("=")[-1]
 
-                try:
-                    comment = Comment(
-                        uscisid=uscisid,
-                        name=c["name"],
-                        comment=c["comment"],
-                        date_posted=c["posted_date"],
-                        date_received=c["received_date"],
-                        traking_number=c["tracking_number"],
-                        rin=c["rin"])
-                    session.add(comment)
-                    session.commit()
-                    print(page, i, uscisid, c)
-                except IntegrityError:  # Do not commit repeated comments.
-                    session.rollback()
+                comment_number = (page - 1)*results_per_page + i
 
-    scraper.shut_down()
+                # Check if entry exists.
+                exists = session.query(Comment).filter(
+                    Comment.uscisid == uscisid).first()
+                if exists is None:
+                    print("GET {}".format(url))
+                    c = scraper.scrape_comment(comment_url=url)
 
-    # Stop xvfb
-    vdisplay.stop()
+                    if c is None:
+                        print("# Bad comment")
+                    else:
+                        try:
+                            comment = Comment(
+                                uscisid=uscisid,
+                                name=c["name"],
+                                comment=c["comment"],
+                                date_posted=c["posted_date"],
+                                date_received=c["received_date"],
+                                traking_number=c["tracking_number"],
+                                rin=c["rin"],
+                                phase="A")
+                            session.add(comment)
+                            session.commit()
+                            print("> ", comment_number, uscisid)
+                        except IntegrityError:  # Do not commit repeated comments.
+                            session.rollback()
+    finally:
+        scraper.shut_down()
+
+        # Stop xvfb
+        vdisplay.stop()
--- public-charge/scraper/scraper.py
+++ public-charge/scraper/scraper.py
@@ -1,6 +1,7 @@
 from math import ceil
 from time import sleep
 from selenium import webdriver
+import selenium
 from xvfbwrapper import Xvfb
 
 
@@ -8,18 +9,18 @@ class Scraper(object):
     """ Scrape regulations.go comments
     """
 
-    def __init__(self, delay=4):
+    def __init__(self, delay=4, results_per_page=50):
         """
         """
         self.delay = delay
+        self.results_per_page = results_per_page
         self.driver = webdriver.Chrome()
         self.driver.set_window_size(1200, 800)
 
     def shut_down(self):
         self.driver.quit()
 
-    @staticmethod
-    def get_page(results_per_page=50, page_number=1):
+    def get_page(self, page_number=1):
         """
         rpp is the results per page (10, 25, 50)
         po is the page offset. If rpp=50, po=0 is 1st page, po=50 is the 2nd 
@@ -32,8 +33,8 @@ class Scraper(object):
         """
 
         url = "https://www.regulations.gov/docketBrowser"
-        url += "?rpp={0}&so=DESC&sb=postedDate".format(results_per_page)
-        url += "&po={0}&dct=PS".format((page_number - 1) * results_per_page)
+        url += "?rpp={0}&so=ASC&sb=postedDate".format(self.results_per_page)
+        url += "&po={0}&dct=PS".format((page_number - 1) * self.results_per_page)
         url += "&D=USCIS-2010-0012"
         return url
 
@@ -44,7 +45,7 @@ class Scraper(object):
         of comments from the bar at the bottom of the page.
         """
         # Get the url for the first page.
-        url = Scraper.get_page(page_number=1)
+        url = self.get_page(page_number=1)
 
         # Open browser.
         self.driver.get(url)
@@ -56,9 +57,12 @@ class Scraper(object):
                 '//*[contains(text(), "Displaying")]').text
             results_available = int(results_available.split()[-1])
         except:
-            results_available = self.driver.find_element_by_xpath(
-                '//div[@class="GIY1LSJILB GIY1LSJBL GIY1LSJIK GIY1LSJCL"]').text
-            results_available = int(results_available.split()[-1])
+            try:
+                results_available = self.driver.find_element_by_xpath(
+                    '//div[@class="GIY1LSJILB GIY1LSJBL GIY1LSJIK GIY1LSJCL"]').text
+                results_available = int(results_available.split()[-1])
+            except:
+                results_available = 0
 
         #total_pages = ceil(results_available / 50)
         return results_available
@@ -76,7 +80,7 @@ class Scraper(object):
             Each entry in the list corresponds to the url for a comment.
         """
         # Get url for given page.
-        url = Scraper.get_page(page_number=page_number)
+        url = self.get_page(page_number=page_number)
 
         # Open browser.
         self.driver.get(url)
@@ -101,16 +105,25 @@ class Scraper(object):
 
         RETURNS
         -------
-        results: dict
+        results: dict (or None on error)
             Name, comment, posted and received date, tracking number, and rin.
         """
         # Go to comment.
-        self.driver.get(comment_url)
-        sleep(self.delay)
+        try:
+            self.driver.get(comment_url)
+            sleep(self.delay)
+        except selenium.common.exceptions.TimeoutException:
+            return None
 
         # Get name.
-        name = self.driver.find_element_by_xpath(
-            '//*[contains(text(), "Comment Submitted by")]').text
+        try:
+            name = self.driver.find_element_by_xpath(
+                '//*[contains(text(), "Comment Submitted by")]').text
+        except selenium.common.exceptions.NoSuchElementException:
+            return None
+        name_prefix = "Comment Submitted by "
+        if name.startswith(name_prefix):
+            name = name[len(name_prefix):]
 
         # Check that text is not empty.
         try:
