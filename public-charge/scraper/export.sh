aa4db261dc2470e54f1edcbc6d2efa31d107ea84
dbg <no>
2018-11-26 21:46:58 -0800

    .

--- public-charge/scraper/export.sh
+++ public-charge/scraper/export.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-batch=${1:?Specify batch}
+batch=$1
 
-query="SELECT * FROM comments WHERE phase = \"${batch}\""
+if [ -z "$batch" ]
+then
+	batch="all"
+	query="SELECT * FROM comments"
+else
+	query="SELECT * FROM comments WHERE phase = \"${batch}\""
+fi
 
 exec sqlite3 -csv -header comments.sqlite "${query}" > ${batch}.csv
